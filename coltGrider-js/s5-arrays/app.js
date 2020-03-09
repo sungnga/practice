336bd40e1bcc021f5d24370aab8bbf4fb88071c7
sungnga <nga422@gmail.com>
2020-03-08 23:38:19 -0700

    Add lessons

---
+++ coltGrider-js/s5-arrays/app.js
@@ -0,0 +1,47 @@
+// GOALS
+// - Work with arrays
+// - Write Object Literals
+// - Understand Reference Types
+// - Use common Array/Object methods    
+
+// ARRAYS
+// ordered collections of values.
+
+
+// CREATING ARRAYS
+
+// To make an empty array
+let students = [];
+new Array(); //[]
+
+// Can initialize an array with data in it
+//An array of strings
+let colors = ['red', 'orange', 'yello'];
+
+//An array of numbers
+let lottoNums [19, 22, 56, 12, 51];
+
+// A mixed array
+let stuff = [true, 68, 'cat', null, NaN];
+
+
+// ARRAY INDICES
+
+// Array indices starts at 0
+// like strings, arrays have .length property
+
+let colors = ['red', 'orange', 'yellow'];
+
+colors[colors.length - 1]; //'yellow'
+
+
+ // MODIFYING ARRAYS
+
+ 
+
+ 
+
+
+
+
+
