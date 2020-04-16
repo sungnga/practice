66ca1a6188e587ba959262604ca115a3a0cd630c
sungnga <nga422@gmail.com>
2020-04-15 23:34:22 -0700

    Add more notes

--- coltGrider-js/s26-ecomm/views/admin/products/new.js
+++ coltGrider-js/s26-ecomm/views/admin/products/new.js
@@ -7,6 +7,7 @@ module.exports = ({ errors }) => {
     // If we don't give a method property to <form>, the default method is GET. The method property is how to share/transmit information to the backend server
     // Default value for enctype="application/x-www-form-urlencoded". A urlencoded means take the data from the form and bundle it into a url-safe string to be transmitted. However the urlencoded doesn't know what to do with the image file
     // with multipart/form-data, the input data is submitted bit by bit
+    // Use getError helper method to print the error message in the browser
     return layout({
         content: `
             <form method="POST" enctype="multipart/form-data">
