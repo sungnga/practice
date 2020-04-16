4e0b878702da88229fc9fd14ec01e072db0245bf
sungnga <nga422@gmail.com>
2020-04-15 23:37:55 -0700

    Better styling

--- coltGrider-js/s26-ecomm/views/admin/products/new.js
+++ coltGrider-js/s26-ecomm/views/admin/products/new.js
@@ -2,22 +2,34 @@ const layout = require('../layout');
 const { getError } = require('../../helpers');
 
 module.exports = ({ errors }) => {
-    // layout() is a function that takes content property
-    // html form is inside a string
-    // If we don't give a method property to <form>, the default method is GET. The method property is how to share/transmit information to the backend server
-    // Default value for enctype="application/x-www-form-urlencoded". A urlencoded means take the data from the form and bundle it into a url-safe string to be transmitted. However the urlencoded doesn't know what to do with the image file
-    // with multipart/form-data, the input data is submitted bit by bit
-    // Use getError helper method to print the error message in the browser
-    return layout({
-        content: `
-            <form method="POST" enctype="multipart/form-data">
-                <input placeholder="Title" name="title" />
-                ${getError(errors, 'title')}
-                <input placeholder="Price" name="price" />
-                ${getError(errors, 'price')}
-                <input type="file" name="image" />
-                <button>Submit</button>
-            </form>
-        `
-    })
+  return layout({
+    content: `
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="subtitle">Create a Product</h1>
+
+          <form method="POST" enctype="multipart/form-data">
+            <div class="field">
+              <label class="label">Title</label>
+              <input class="input" placeholder="Title" name="title">
+              <p class="help is-danger">${getError(errors, 'title')}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Price</label>
+              <input class="input" placeholder="Price" name="price">
+              <p class="help is-danger">${getError(errors, 'price')}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Image</label>            
+              <input type="file" name="image" />
+            </div>
+            <br />
+            <button class="button is-primary">Create</button>
+          </form>
+        </div>
+      </div>
+    `
+  });
 };
