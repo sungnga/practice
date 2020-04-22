30c8780ac44abae217138824d1f7e1bf0b7e675b
sungnga <nga422@gmail.com>
2020-04-22 13:46:35 -0700

    Reading and writing json files exercise

--- mead-nodejs/playground/1-json.js
+++ mead-nodejs/playground/1-json.js
@@ -1,29 +1,29 @@
 const fs = require('fs')
 
-// const book = {
-//     title: 'Ego is the Enemy',
-//     author: 'Ryan Holiday'
-// }
+const book = {
+    title: 'Ego is the Enemy',
+    author: 'Ryan Holiday'
+}
 
-// // A JS method that takes a JS object and returns a json string representation
-// const bookJSON = JSON.stringify(book)
-// // 1st arg: create a json file
-// // 2nd arg: the data we want to write
-// fs.writeFileSync('1-json.json', bookJSON)
-// console.log(bookJSON)
+// A JS method that takes a JS object and returns a json string representation
+const bookJSON = JSON.stringify(book)
+// 1st arg: create a json file
+// 2nd arg: the data we want to write
+fs.writeFileSync('1-json.json', bookJSON)
+console.log(bookJSON)
 
-// // The opposite of JSON.stringify() method
-// // Takes a json string and returns an object
-// const parsedData = JSON.parse(bookJSON)
-// console.log(parsedData.author)
+// The opposite of JSON.stringify() method
+// Takes a json string and returns an object
+const parsedData = JSON.parse(bookJSON)
+console.log(parsedData.author)
 
-// // What comes back is a buffer, not a string
-// const dataBuffer = fs.readFileSync('1-json.json')
-// // Call toString() method on the buffer to get a string represenation
-// const dataJSON = dataBuffer.toString()
-// // Parse the json data to an object
-// const data = JSON.parse(dataJSON)
-// console.log(data.title)
+// What comes back is a buffer, not a string
+const dataBuffer = fs.readFileSync('1-json.json')
+// Call toString() method on the buffer to get a string represenation
+const dataJSON = dataBuffer.toString()
+// Parse the json data to an object
+const data = JSON.parse(dataJSON)
+console.log(data.title)
 
 
 //
