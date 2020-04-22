776755a9291513fbce18b55b99f0f017bec1db07
sungnga <nga422@gmail.com>
2020-04-21 23:23:29 -0700

    Printing in colors using chalk package

--- mead-nodejs/s3-notes_app/app.js
+++ mead-nodejs/s3-notes_app/app.js
@@ -1,17 +1,18 @@
-const validator = require('validator');
+// const validator = require('validator');
 const getNotes = require('./notes.js');
+const chalk = require('chalk');
 
 const msg = getNotes();
 console.log(msg);
 
-console.log(validator.isURL('https://mead.io'))
-
+console.log(chalk.green.bold.inverse('Success!'));
 
 
 
 // =============
 // NOTES
 // =============
+// NPM PACKAGES
 // 2 steps to use npm packages in an application
 // 1. Run 'npm init' in the project directory to initialize npm.
 // This will create a package.json file. This file is used to manage all the dependencies that our application needs to run. Here we list all the npm packages we want to use
@@ -19,3 +20,7 @@ console.log(validator.isURL('https://mead.io'))
 // It has the documenation of how to use the package, how to install it, and version number
 // Example of installing a npm package: npm install validator@13.0.0
 // Need to require in the npm package in order to use its functionalities
+
+// TO RECREATE THE NODE MODULE FOLDER
+// Run 'npm install' in the application directory
+// It will create the node module folder base off of the content in the 'package-lock.json' and 'package.json' files
