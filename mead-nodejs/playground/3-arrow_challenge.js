816ebc9ef97319aa441029c20d1d60d665268ded
sungnga <nga422@gmail.com>
2020-04-23 16:49:27 -0700

    Arrow function exercises

---
+++ mead-nodejs/playground/3-arrow_challenge.js
@@ -0,0 +1,35 @@
+//
+// Goal: Create method to get incomplete tasks
+//
+// 1. Define getTasksToDo method
+// 2. Use filter to to return just the incompleted tasks (arrow function)
+// 3. Test your work by running the script
+
+const tasks = {
+    tasks: [{
+        text: 'Grocery shopping',
+        completed: true
+    },{
+        text: 'Clean yard',
+        completed: false
+    }, {
+        text: 'Film course',
+        completed: false
+        }],
+    getTasksToDo() {
+        // const incompletedTasks = this.tasks.filter((task) => {
+        //     return task.completed === false
+        // })
+        // return incompletedTasks
+
+        // A more concise version
+        // return this.tasks.filter((task) => {
+        //     return task.completed === false
+        // })
+
+        // A even more concise version!!
+        return this.tasks.filter((task) => task.completed === false)
+    }
+}
+
+console.log(tasks.getTasksToDo())
