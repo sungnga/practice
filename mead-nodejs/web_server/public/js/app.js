78a0f7c5f7c7a3861cfad124482b885cd365b12e
sungnga <nga422@gmail.com>
2020-04-26 12:27:52 -0700

    Create a search form and wire up the UI

--- mead-nodejs/web_server/public/js/app.js
+++ mead-nodejs/web_server/public/js/app.js
@@ -1,18 +1,41 @@
-console.log('Client side javascript file is loaded!')
+// fetch('http://localhost:3000/weather?address=seattle')
+// .then((response) => {
+//     response.json().then((data) => {
+//         if (data.error) {
+//             console.log(data.error)
+//         } else {
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
 
-fetch('http://localhost:3000/weather?address=seattle')
-.then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+//     })
+// })
 
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    // To prevent the default behavior of refreshing the browser upon clicking the submit button
+    e.preventDefault()
+    //.value extracts the input value and stores in location variable
+    const location = search.value
+
+    messageOne.textContent = 'Loading'
+    messageTwo.textContent = ''
+    
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
     })
 })
-
 
 
 // BROWSER HTTP REQUEST WITH FETCH
