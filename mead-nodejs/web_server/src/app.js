23b3c1620303ac464324d41391d89a20d9874683
sungnga <nga422@gmail.com>
2020-04-26 13:45:11 -0700

    Setup app for Heroku

--- mead-nodejs/web_server/src/app.js
+++ mead-nodejs/web_server/src/app.js
@@ -7,6 +7,7 @@ const forecast = require('./utils/forecast')
 
 // Creates an Express application
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -110,8 +111,8 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 }) 
 
 // GOAL: WIRE UP /weather
