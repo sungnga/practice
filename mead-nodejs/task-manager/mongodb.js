32a87062a0910df0f61c971a56516b005d0d1c09
sungnga <nga422@gmail.com>
2020-04-27 12:38:13 -0700

    Inserting multiple documents at once

--- mead-nodejs/task-manager/mongodb.js
+++ mead-nodejs/task-manager/mongodb.js
@@ -1,42 +1,61 @@
 // CRUD create read update delete
 
-// What comes back from the mongodb npm library is an object
-// This is a native driver created by the mongodb company, allowing us to connect to a mongodb database from node.js
 const mongodb = require('mongodb')
-// The MongoClient gives us access to the function necessary to connect to the database so we can perform our four basic CRUD operations
 const MongoClient = mongodb.MongoClient
 
-// Define the connection URL and the database we're trying to connect to
-// url: the local host that is up and running
-// mongodb:// is mongodb's protocol
-// 127.0.0.1 is the local host ip
-// :27017 is the port #
 const connectionURL = 'mongodb://127.0.0.1:27017'
-// Can name it whatever you want
 const databaseName = 'task-manager'
 
-// To setup the connection
-// The .connect() method is used to connect to a specific server (connectionURL)
-// The callback function gets called once we're connected to the database
-// This is an async operation
-// Once the connection is open and remains active, the node process continues to stay up and running. Cntrl c to end the process
 MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client) => {
-    // Return early if an error occurs
     if (error) {
         return console.log('Unable to connect to database')
     }
 
-    // What you get back when calling client.db() is a reference to a specific database you want to manipulate
-    // The variable db is the ref to the database
     const db = client.db(databaseName)
 
-    // To insert a single document into a collection
-    // .collection() is a function and it expects the NAME of the collection you're trying to manipulate
-    // We can call a method on that collection reference to insert a document: .insertOne()
-    // .insertOne() expects the 1st arg as an object, which contains all the data you try to insert
-    db.collection('users').insertOne({
-        name: 'Andrew',
-        age: 27
+    // db.collection('users').insertOne({
+    //     name: 'Andrew',
+    //     age: 27
+    // }, (error, result) => {
+    //         if (error) {
+    //             return console.log('Unable to insert user')
+    //         }
+            
+    //         console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, {
+    //         name: 'Brad',
+    //         age: 26
+    //     }
+    // ], (error, result) => {
+    //         if (error) {
+    //             return console.log('Unable to insert documents!')
+    //         }
+
+    //         console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'wash dishes',
+            completed: true
+        }, {
+            description: 'laundry',
+            completed: false
+        }, {
+            description: 'cook',
+            completed: true
+        }
+    ], (error, result) => {
+            if (error) {
+                return console.log('Unable to insert tasks')
+            }
+            console.log(result.ops)
     })
 })
 
@@ -71,11 +90,46 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 //  - what you get back when calling client.db() is a reference to a specific database you want to manipulate
 //  - the variable db is the ref to the database
 
-// To insert a single document into a collection:
+// Inserting documents into a collection:
 // db.collection('users').insertOne({
 //     name: 'Andrew',
 //     age: 27
 // })
 //  - .collection() is a function and it expects the NAME of the collection you're trying to manipulate
 //  - we can call a method on that collection reference to insert a document: .insertOne()
-//  - .insertOne() expects the 1st arg be an object, which contains all the data you try to insert. The data in the object is called field
+
+// Insert a single document with .insertOne() method:
+//  - .insertOne() is an async operation, so we need to handle any errors or confirm that the operation works as expected
+//  - .insertOne() expects the 1st arg be an object, which contains all the data you try to insert. Each property in the object is a called field
+//  - 2nd arg: is a callback function which is going to get called when the operation is complete. It gets called one of two potential arguments: 
+//    - an error, if things went poorly
+//    - result, if things went well. Result contains THE DATA & THE UNIQUE ID THAT IS ASSIGNED TO THE DOCUMENT(the individual items)
+// result.ops contains an array of documents
+// db.collection('users').insertOne({
+//     name: 'Andrew',
+//     age: 27
+// }, (error, result) => {
+//         if (error) {
+//             return console.log('Unable to insert user')
+//         }    
+//         console.log(result.ops)
+// })
+
+// Insert multiple documents with .insertMany() method:
+//  - 1st arg: is an array which contains a list of documents(items) you want to insert
+//  2nd arg: is a callback function that takes 2 arguments: an error and a result(has the data and unique id)
+// db.collection('users').insertMany([
+//     {
+//         name: 'Jen',
+//         age: 28
+//     }, {
+//         name: 'Brad',
+//         age: 26
+//     }
+// ], (error, result) => {
+//         if (error) {
+//             return console.log('Unable to insert documents!')
+//         }
+//         console.log(result.ops)
+// })
+
