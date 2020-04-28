e005b589810d8454d5afeb7a8e24d9acfa574841
sungnga <nga422@gmail.com>
2020-04-28 11:54:19 -0700

    Setup mongoose and create a mongoose model

---
+++ mead-nodejs/task-manager/src/db/mongoose.js
@@ -0,0 +1,111 @@
+const mongoose = require('mongoose')
+
+// Connecting mongoose to the mongodb database
+// The same localhost and port number as mongodb
+// task-manager-api is the name of the database
+// The collections will be saved to this database
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+// Creating a new model
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+// Create an instance from the model
+const me = new User({
+    name: 'Nga',
+    age: 99
+})
+
+const task = new Task({
+    description: "laundry",
+    completed: false
+})
+
+// The save() method doesn't take any args and returns a promise
+me.save()
+.then(() => {
+    console.log(me)
+})
+.catch((error) => {
+    console.log('Error!', error)
+})
+
+task.save()
+.then(() => {
+    console.log(laundry)
+})
+.catch((error) => {
+    console.log('Error!', error)
+})
+
+
+// ======================
+// MONGOOSE
+// ======================
+// https://mongoosejs.com/
+// Elegant mongodb object modeling for node.js
+
+// CONNECTING MONGOOSE TO THE MONGODB DATABASE:
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// })
+// The same localhost and port number as mongodb
+// task-manager-api is the name of the database
+// The collections will be saved to this database
+
+// CREATE A MODEL
+// Create a new model by calling the mongoose.model() function. It takes 2 arguments
+// 1st arg: the name of the model in string format. Uppercase the first letter
+// 2nd arg: an object which define the properties and their types of the model
+// Setting the constructor function:
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String
+//     },
+//     age: {
+//         type: Number
+//     }
+// })
+
+// Instantiating an instance of the model:
+// By calling the User function with: new User()
+// The new instance/user is called 'me'
+// Pass in an object that contains the data for this particular user
+// const me = new User({
+//     name: 'Nga',
+//     age: 99
+// })
+
+// To save the instance to the collection:
+// Call the .save() method on the instance
+// save() method returns a PROMISE
+// Chain on the .then() and .catch() methods to resolve or reject the promise with a callback function
+// me.save()
+// .then(() => {
+//     console.log(me)
+// })
+// .catch((error) => {
+//     console.log('Error!', error)
+// })
