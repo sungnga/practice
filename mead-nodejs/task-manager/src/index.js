266569d863c8e0b9ba0f4d17febef192084b611a
sungnga <nga422@gmail.com>
2020-04-29 08:58:58 -0700

    Convert all Express app handlers to async/await

--- mead-nodejs/task-manager/src/index.js
+++ mead-nodejs/task-manager/src/index.js
@@ -12,95 +12,143 @@ app.use(express.json())
 // Setup a REST API route, the route for creating a new user
 // Setup the resource creation endpoint: /users
 // To test the url in postman: localhost:3000/users
-app.post('/users', (req, res) => {
-    // req.body contains the json response we get back
-    // console.log(req.body)
-
-    // Create a new user from the User model
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then(() => {
-        // if the promise is resolved, save the user data to the database
-        // sending the user a more detailed status code
+    // Use a try/catch block to handle errors if the promise is rejected
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((e) => {
-        // call the .status() method and pass in the status code
-        // then call the .send() method to send the error value
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
+
+    // promise chaining is replaced with async/await
+    // user.save().then(() => {
+    //     res.status(201).send(user)
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
     
 })
 
-// Setup a REST API route handler for creating a new task
-// Setup the resource creation endpoint: /tasks
-app.post('/tasks', (req, res) => {
-    // console.log(req.body)
-    const task = new Task(req.body)
-
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
-
-app.listen(port, () => {
-    console.log('Server is up on port' + port)
-})
-
 // Setting up route handler for fetching all users
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch((e) => {
-        // status code of 500 is internal server error
+    } catch (e) {
         res.status(500).send()
-    })
+    }
+
+    // promise chaining is replaced with async/await
+    // User.find({}).then((users) => {
+    //     res.send(users)
+    // }).catch((e) => {
+    //     // status code of 500 is internal server error
+    //     res.status(500).send()
+    // })
 })
 
 // Fetching a user by its id
-app.get('/users/:id', (req, res) => {
-    // req.params contains the value that the user passed in in place of the :id 
-    // What's returned is an object that contains the property name (in this case, id) and the value the user provided
-    // { id: 12345 }
-    console.log(req.params)
+app.get('/users/:id', async (req, res) => {
+    // console.log(req.params)
 
     // The id that the user provides
     const _id = req.params.id
+        
+    try {
+        const user = await User.findById(_id)
 
-    User.findById(_id).then((user) => {
         if (!user) {
             return res.status(404).send()
         }
 
         res.send(user)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
+
+    // promise chaining is replaced with async/await
+    // User.findById(_id).then((user) => {
+    //     if (!user) {
+    //         return res.status(404).send()
+    //     }
+
+    //     res.send(user)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
+})
+
+// Setup a REST API route handler for creating a new task
+// Setup the resource creation endpoint: /tasks
+app.post('/tasks', async (req, res) => {
+    // console.log(req.body)
+    const task = new Task(req.body)
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+
+    // promise chaining is replaced with async/await
+    // task.save().then(() => {
+    //     res.status(201).send(task)
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
 })
 
 // Fetching all tasks
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
+
+    // promise chaining is replaced with async/await
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
 // Fetching a task by its id
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
+    
+    try {
+        const task = await Task.findById(_id)
 
-    Task.findById(_id).then((task) => {
         if (!task) {
             return res.status(404).send()
         }
 
         res.send(task)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
+
+    // promise chaining is replaced with async/await
+    // Task.findById(_id).then((task) => {
+    //     if (!task) {
+    //         return res.status(404).send()
+    //     }
+
+    //     res.send(task)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port' + port)
 })
 
 // ==================
@@ -160,3 +208,24 @@ app.get('/tasks/:id', (req, res) => {
 //         res.status(500).send()
 //     })
 // })
+
+// CONVERTING THE EXPRESS APP ROUTE HANDLERS TO ASYNC/AWAIT
+// Mark the function as an async function by using the 'async' keyword in front of the function declaration
+// Use the try/catch block to catch any errors if the async operation failed
+// Run the code in the try block if the promise is resolved
+// Run the catch block if the promise is rejected
+// app.get('/tasks/:id', async (req, res) => {
+//     const _id = req.params.id
+//   
+//     try {
+//         const task = await Task.findById(_id)
+//
+//         if (!task) {
+//             return res.status(404).send()
+//         }
+//
+//         res.send(task)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// }
