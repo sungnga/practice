a3301e59dc83c4f0e1552241064c86887be2f17e
sungnga <nga422@gmail.com>
2020-04-29 08:08:32 -0700

    Async/await exercises

--- mead-nodejs/task-manager/playground/promise_chaining2.js
+++ mead-nodejs/task-manager/playground/promise_chaining2.js
@@ -1,11 +1,29 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
+// Using promise chaining
+// A function that deletes a task by its id
+// Then count all the tasks that is marked not complete
 Task.findByIdAndDelete('5ea887232e3bc6722274f5b6').then((task) => {
     console.log(task)
     return Task.countDocuments({completed: false})
 }).then((list) => {
     console.log(list)
+}).catch((e) => {
+    console.log(e)
+})
+
+// Using async/await
+// A function that deletes a task by its id
+// Then count all the tasks that have not been completed
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('5ea8da92c985e0863511aa77').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
