9daf96cd1501ee08f728305ba1b4571fa5af9ba5
sungnga <nga422@gmail.com>
2020-04-28 21:19:55 -0700

    Resource reading endpoints

--- mead-nodejs/task-manager/src/index.js
+++ mead-nodejs/task-manager/src/index.js
@@ -48,8 +48,60 @@ app.listen(port, () => {
     console.log('Server is up on port' + port)
 })
 
+// Setting up route handler for fetching all users
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        // status code of 500 is internal server error
+        res.status(500).send()
+    })
+})
 
+// Fetching a user by its id
+app.get('/users/:id', (req, res) => {
+    // req.params contains the value that the user passed in in place of the :id 
+    // What's returned is an object that contains the property name (in this case, id) and the value the user provided
+    // { id: 12345 }
+    console.log(req.params)
 
+    // The id that the user provides
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+// Fetching all tasks
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+// Fetching a task by its id
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
 
 // ==================
 // NOTES
@@ -85,5 +137,26 @@ app.listen(port, () => {
 //         // call the .status() method and pass in the status code
 //         // then call the .send() method to send the error value
 //         res.status(400).send(e)
+//     })
+// })
+
+// Fetching a user by its id:
+// app.get('/users/:id', (req, res) => {
+//     // req.params contains the value that the user passed in in place of the :id 
+//     // What's returned is an object that contains the property name (in this case, id) and the value the user provided
+//     // { id: 12345 }
+//     console.log(req.params)
+//
+//     // The id that the user provides
+//     const _id = req.params.id
+//
+//     User.findById(_id).then((user) => {
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+//
+//         res.send(user)
+//     }).catch((e) => {
+//         res.status(500).send()
 //     })
 // })
