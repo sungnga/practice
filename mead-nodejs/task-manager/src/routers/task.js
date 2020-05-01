0f342b78bf978168af208696939b4ea50af21e21
sungnga <nga422@gmail.com>
2020-05-01 09:42:09 -0700

    Sorting data

--- mead-nodejs/task-manager/src/routers/task.js
+++ mead-nodejs/task-manager/src/routers/task.js
@@ -21,28 +21,46 @@ router.post('/tasks', auth, async (req, res) => {
 
 // GET /tasks?completed=true
 // GET /tasks?limit=3&skip=0
-// Use limit and skip to paginate your search result
-// limit: to limit the number of results we get back for any given request
-// skip: allows you to iterate over pages
+// GET /tasks?sortBy=createdAt:desc
 // mongoose enable these features under options property
 router.get('/tasks', auth, async (req, res) => {
     // Start out with an empty match object
     const match = {}
+    // Start out with an empty sort object
+    const sort = {}
 
     // Check to see if the user provides additional query at endpoint
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
     }
 
+    if (req.query.sortBy) {
+        // Split the value of sortBy query
+        const parts = req.query.sortBy.split(':')
+        // Ternary operation
+        // If condition is true, set to - 1 for sort by descending order
+        // If condition is false, set to 1 for sort by ascending order
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     // The final match object gets passed into the .populate() method as a filter property
     // parseInt() method will parse the string into a number
+    // Use limit and skip options to paginate your search result
+    // limit: to limit the number of results we get back for any given request
+    // skip: allows you to iterate over pages
+    // sort: allows you to sort the result you get back
+    // Use sort option to sort the result of your search
+    // Can sort documents by its field names. Ascending order = 1. Descending order = -1
+    //  - Example: sort: { createAt: -1 } to sort by descending ordder of when it was created
+    // Here, the final sort object gets passed into the options property
     try {
         await req.user.populate({
             path: 'userTasks',
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
         res.send(req.user.userTasks)
