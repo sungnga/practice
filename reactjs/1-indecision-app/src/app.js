1dada6bcf0d846d0521b1696286cfd0d39d72cc0
sungnga <nga422@gmail.com>
2020-05-06 16:16:41 -0700

    Picking an option with Math.random()

--- reactjs/1-indecision-app/src/app.js
+++ reactjs/1-indecision-app/src/app.js
@@ -25,6 +25,13 @@ const onRemoveAll = () => {
     render()
 }
 
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length)
+    const option = app.options[randomNum]
+    alert(option)
+    render()
+}
+
 const appRoot = document.querySelector('#app')
 
 // We can inject an array into JSX: {[...]}
@@ -37,7 +44,7 @@ const render = () => {
             <h1>{app.title}</h1>
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-            <p>{app.options.length}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
             <button onClick={onRemoveAll}>Remove All</button>
             <ol>
                 {
