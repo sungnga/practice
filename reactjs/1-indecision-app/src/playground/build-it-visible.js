9c576343f2eac6327da96d570658ca54e3c26a0f
sungnga <nga422@gmail.com>
2020-05-07 15:47:37 -0700

    Adding component state to Visibility Toggle

--- reactjs/1-indecision-app/src/playground/build-it-visible.js
+++ reactjs/1-indecision-app/src/playground/build-it-visible.js
@@ -1,19 +1,58 @@
-let visibility = false
+// ***************************************************
+// Visibility Toggle using React Component and State
+// ***************************************************
+// VisibilityToggle - render, constructor, handleToggleVisibility
+// visibility -> false
 
-const toggle = () => {
-    visibility = !visibility
-    render()
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this)
+        this.state = {
+            visibility: false
+        }
+    }
+
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            }
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide details' : 'Show details'}</button>
+                {this.state.visibility && <p>Devil is in the details</p>}
+            </div>
+        )
+    }
 }
 
-const render = () => {
-    const visible = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggle}>{visibility ? 'Hide details' : 'Show details'}</button>
-            {visibility && <p>The devil is in the detail</p>}
-        </div>
-    )
+ReactDOM.render(<VisibilityToggle />, document.querySelector('#app'))
 
-    ReactDOM.render(visible, document.querySelector('#app'))
-}
-render()
+// **************************
+// Visibility Toggle using JSX
+// **************************
+
+// let visibility = false
+
+// const toggle = () => {
+//     visibility = !visibility
+//     render()
+// }
+
+// const render = () => {
+//     const visible = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={toggle}>{visibility ? 'Hide details' : 'Show details'}</button>
+//             {visibility && <p>The devil is in the detail</p>}
+//         </div>
+//     )
+
+//     ReactDOM.render(visible, document.querySelector('#app'))
+// }
+// render()
