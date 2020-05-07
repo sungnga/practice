50af25ab87afe6f371db0c28905e5ee185ae2cb0
sungnga <nga422@gmail.com>
2020-05-07 11:38:12 -0700

    Component props

--- reactjs/1-indecision-app/src/app.js
+++ reactjs/1-indecision-app/src/app.js
@@ -1,13 +1,24 @@
 // Create a parent component where all other components will be nested in
 // Since a component renders JSX, it can render other components
 // This allows us to nest components inside another by referencing it
+// COMPONENT PROPS: 
+//  - Component props allows components to comnunicate with one another
+//  - To do that, we pass data in when we initialize/define the instance of a component. for example: <Header />
+//  - That data is known as props
+//  - Setting up component props is similar to setting up html attributes
+//  - Setting key/value pair: <Header title="Test value" />
+//  - To display the props, go to the Component and use this.props.keyName inside the JSX: <h1>{this.props.title}</h1>
 class IndecisionApp extends React.Component {
     render() {
+        const title = 'Indecision'
+        const subtitle = 'Put your life in the hands of a computer'
+        const options = ['thing one', 'thing two', 'thing three']
+
         return (
             <div>
-                <Header />
+                <Header title={title} subtitle={subtitle} />
                 <Action />
-                <Options />
+                <Options options={options} />
                 <AddOption />
             </div>
         )
@@ -19,12 +30,18 @@ class IndecisionApp extends React.Component {
 // Now Header is a React component. It has all the features of React
 // React components require one method to be defined. It is a special method that it calls. render()
 // When Header component calls rend(), it returns JSX
+// To use component props:
+//  - inside a component class, we have access to the 'this' keyword, which is a reference to the current instance of this component
+//  - React gives us access to the instance's props on an object(comes in as key/value pair): this.props
+//  - the props key is the name of the attribute in JSX when we initialize/define the instance: <Header title={title} subtitle={subtitle} />
+//  - to display this props inside a JSX: <h1>{this.props.title}</h1>
 class Header extends React.Component {
     render() {
+        //console.log(this.props)
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of a computer</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         )
     }
@@ -42,24 +59,29 @@ class Action extends React.Component {
     }
 }
 
+// this.props.options is the props data that was defined when the instance of Options component was initialized: <Options options={options} />
+// In this case, options is an array of items
+// .map() over this options array to get the individual items
+// Then pass each item to the instance of Option component as a props data when it's initialize here. <Option key={option} optionText={option} />)
 class Options extends React.Component {
     render() {
         return (
             <div>
-                Options component here
-                <Option />
-                <Option />
+                {
+                    this.props.options.map((option) => <Option key={option} optionText={option} />)
+                }
             </div>
         )
     }
 }
 
-// This component is nest inside the Options component
+// This component is nested inside the Options component
+// <div>{this.props.optionText}</div> displays the props data that was defined in the Option component instance when it was initialized
 class Option extends React.Component {
     render() {
         return (
             <div>
-                Option component here
+                {this.props.optionText}
             </div>
         )
     }
@@ -86,4 +108,7 @@ class AddOption extends React.Component {
 //         <AddOption />
 //     </div>
 // )
+
+// To render the parent component, initialize the parent component here
+// <IndecisionApp />
 ReactDOM.render(<IndecisionApp />, document.querySelector('#app'))
