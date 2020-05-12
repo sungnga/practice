e3f0ff242bcac07de4d48a51e5474d04a00015a7
sungnga <nga422@gmail.com>
2020-05-11 23:08:37 -0700

    Linking between routes

--- reactjs/expensify-app/src/app.js
+++ reactjs/expensify-app/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter, Route, Switch } from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
@@ -30,19 +30,32 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
     <div>
-        404!
+        404 - <Link to="/">Go home</Link>
     </div>
+)
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </header>
 )
 
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true} />
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage} />
-        </Switch>
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit" component={EditExpensePage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
     </BrowserRouter>
 )
 
@@ -57,7 +70,8 @@ ReactDOM.render(routes, document.querySelector('#app'))
 // Source: https://reacttraining.com/react-router/
 //
 // Install the react-router for web: npm install react-router-dom
-// Import into app.js file and destructure items we want to use: import {BrowserRouter, Route} from 'react-router-dom'
+// Import into app.js file and destructure items we want to use: 
+// import { BrowserRouter, Route } from 'react-router-dom'
 // To create the router configuration:
 //  - Only use a single instance of BrowserRouter
 //  - Inside the BrowserRouter, set up as many instances of Route as pages we have
@@ -79,7 +93,8 @@ ReactDOM.render(routes, document.querySelector('#app'))
 // To fix this, need to configure the dev server in the webpack.config.js file, telling the dev server to always serve up the index.html file for all 404 routes: historyApiFallback: true
 
 // Setting up a 404 page:
-//  - Import the Switch instance from react-router-dom: import {BrowserRouter, Route, Switch } from 'react-router-dom'
+//  - Import Switch from react-router-dom: 
+//  - import { Switch } from 'react-router-dom'
 //  - Switch will go through each Route one by one to see if the path matches with the requested path
 //  - If Switch finds a matched path, it will stop looking
 //  - The last Route inside Switch is a 404 not found component. This component gets rendered if the path does not match
@@ -92,3 +107,12 @@ ReactDOM.render(routes, document.querySelector('#app'))
     //         <Route component={NotFoundPage} />
     //     </Switch>
     // </BrowserRouter>
+
+// Linking between routes: Link and NavLink
+//  - Import Link and NavLink from react-router-dom: 
+//  - import { Link, NavLink } from 'react-router-dom'
+//  - Use Link when we want to change or switch between pages/routes
+//  - The nice thing about Link is that we're using client-side routing as oppose to server-side routing
+//  - This means that it's not going through the full page refresh. Intead, Javascript just swaps things out on the fly. It makes a new call to ReactDOM.render() to render the new page
+//  - Use Link whenever we want to take advantage of client-side routing instead of using an anhcor tag
+//  - Use NavLink for navigation. And we can call out that specific link that we're on
