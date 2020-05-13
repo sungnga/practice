f106d6ab42bd9e15598db75dbe4c63ba1caf6465
sungnga <nga422@gmail.com>
2020-05-12 17:52:12 -0700

    ES6 array destructuring

--- reactjs/expensify-app/src/playground/ES6-destructuring.js
+++ reactjs/expensify-app/src/playground/ES6-destructuring.js
@@ -1,4 +1,6 @@
+//
 // ES6 OBJECT DESTRUCTURING
+//
 // We can destructure objects
 // We can pull things off of objects and create separate variables for those values
 // What is destructuring do?
@@ -35,6 +37,28 @@ const book = {
         name: 'Penguin'
     }
 }
-
 const {name: publisherName = 'Self-Published'} = book.publisher
 console.log(publisherName)
+
+
+
+//
+// ES6 ARRAY DESTRUCTURING
+//
+// Just like object destructuring allows us to pull properties off of an object, array destructuring allows us to pull items off an array
+
+const address = ['123 S June Street', 'San Francisco', 'California', '98034']
+
+// For object destructuring, we use curly braces
+// For array destructuring, we use square brackets
+// And then we pick the thing that we want to destructure: the address
+// What goes inside the square bracket is an ordered list of variable names
+// These variable names is matching up by the position of the array
+// You don't need to destructure all items in the array. Make sure to use commas to identify the position
+// Like objects, you can set default values
+const [, cityName, state = 'Seattle', zip] = address
+console.log(`You are in ${state}`)
+
+const item = ['coffee (hot)', '$2.00', '$2.50', '$3.00']
+const [itemName, , medium, large] = item
+console.log(`A medium ${itemName} costs ${medium}`)
