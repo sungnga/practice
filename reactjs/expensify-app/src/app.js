8d905539f62cd2c10e5e1243fd39f40518107215
sungnga <nga422@gmail.com>
2020-05-13 11:36:39 -0700

    Intro to Reducers

--- reactjs/expensify-app/src/app.js
+++ reactjs/expensify-app/src/app.js
@@ -201,3 +201,39 @@ ReactDOM.render(<AppRouter />, document.querySelector('#app'))
     // store.dispatch(incrementCount())
     // // Action property is provided
     // store.dispatch(incrementCount({incrementBy: 5}))
+
+// REDUCERS
+// 1. Reducers are pure functions
+//  - the output is only determined by the input. What it returns, it is only determined by the things that get passed in
+//  - it doesn't use anything else from outside of the function scope and it doesn't change anything outside of the function scope either
+//  - we don't want to change variables outside of the reducer's scope
+//  - and we don't want to rely on values from variables outise of the reducer's scope
+//  - we just want to use the input the state and the action to return the new state value
+// 2. Never change state or action
+//  - mutating the state directly is going to have undesired effects
+    // // A reducer
+    // const countReducer = (state = { count: 0 }, action) => {
+    //     switch (action.type) {
+    //         case 'INCREMENT':
+    //             return {
+    //                 count: state.count + action.incrementBy
+    //             }
+    //         case 'DECREMENT':
+    //             return {
+    //                 count: state.count - action.decrementBy
+    //             }
+    //         case 'RESET':
+    //             return {
+    //                 count: 0
+    //             };
+    //         case 'SET':
+    //             return {
+    //                 count: action.count
+    //             }
+    //         default: 
+    //             return state
+    //     }
+    // }
+        
+    // // Make a store
+    // const store = createStore(countReducer)
