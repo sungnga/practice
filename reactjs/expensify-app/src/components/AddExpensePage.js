a013ed8cbf023c0e5c17b36cee8ba516d738d3e0
sungnga <nga422@gmail.com>
2020-05-14 23:22:58 -0700

    Wiring up add expense

--- reactjs/expensify-app/src/components/AddExpensePage.js
+++ reactjs/expensify-app/src/components/AddExpensePage.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import {addExpense} from '../actions/expenses'
 
-const AddExpensePage = () => (
+const AddExpensePage = (props) => (
     <div>
         <h1>Add Expense</h1>
-        <ExpenseForm />
+        <ExpenseForm
+            onSubmit={(expense) => {
+                props.dispatch(addExpense(expense))
+                props.history.push('/')
+            }}
+        />
     </div>
-)
+);
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
+
+
+// =================
+// NOTES
+// =================
+
+// The AddExpensePage component starts out as a regular component
+//  - It renders the ExpenseForm instance (an expense form) to the page
+// The AddExpensePage needs to be able to dispatch the given action to the Redux store
+// To do this we need to connect the component to the store using connect()
+//  - import {connect} from 'react-redux'
+//  - pass the component to connect: export default connect()(AddExpensePage);
+// Now we have access to props.dispatch()
+//  - pass in the props to the component: const AddExpensePage = (props) => (..)
+//  - to use the dispatch: props.dispatch()
+// We have an action generator already, so we just need to import it
+//  - we're using addExpense action to add an expense to the store
+//  - import { addExpense } from '../actions/expenses'
+//  - to dispatch the action: props.dispatch(addExpense(expense))
+// To redirect to a different page after submit, use props.history.push('/')
--- reactjs/expensify-app/src/components/ExpenseForm.js
+++ reactjs/expensify-app/src/components/ExpenseForm.js
@@ -15,7 +15,8 @@ export default class ExpenseForm extends React.Component {
         note: '',
         amount: '',
         createdAt: moment(),
-        calendarFocused: false
+        calendarFocused: false,
+        error: ''
     };
     onDescriptionChange = (e) => {
         const description = e.target.value;
@@ -27,20 +28,51 @@ export default class ExpenseForm extends React.Component {
     };
     onAmountChange = (e) => {
         const amount = e.target.value;
-        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+        if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }));
         }
     };
     onDateChange = (createdAt) => {
-        this.setState(() => ({ createdAt }));
+        // If there is a date provided, then set the date value
+        // Else, do nothing
+        // This prevents the user from clearing the date value
+        if (createdAt) {
+            this.setState(() => ({ createdAt }));
+        }
     };
     onFocusChange = ({ focused }) => {
         this.setState(() => ({ calendarFocused: focused }));
     };
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        if (!this.state.description || !this.state.amount) {
+            // Set error state to equal to 'Please provide description and amount'
+            this.setState(() => ({ error: 'Please provide description and amount' }))
+        } else {
+            // Clear the error
+            this.setState(() => ({ error: '' }))
+            // onSubmit prop gets passed down from the ExpenseForm instance, which was defined in AddExpensePage.js
+            // Here we define what we want to submit. We submit an object
+            this.props.onSubmit({
+                description: this.state.description,
+                // Convert the string amt to number
+                // parseFloat keeps decimal in place
+                // 10, because we're working in base 10
+                // Multiply by 100 to get dollar amt instead of cents
+                amount: parseFloat(this.state.amount, 10) * 100,
+                // Currently, createAt is a Moment object
+                // There's a Moment method we can use to get the timestamp back: .valueOf()
+                createdAt: this.state.createdAt.valueOf(),
+                note: this.state.note
+            })
+        }
+    };
     render() {
         return (
             <div>
-                <form>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.onSubmit}>
                     <input
                         type="text"
                         placeholder="Description"
