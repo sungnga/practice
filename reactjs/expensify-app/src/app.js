a4bd34096967b31d7675c35f1a54fd8d622ece87
sungnga <nga422@gmail.com>
2020-05-16 11:28:48 -0700

    Remove dummy expenses data

--- reactjs/expensify-app/src/app.js
+++ reactjs/expensify-app/src/app.js
@@ -12,17 +12,6 @@ import './styles/styles.scss'
 const store = configureStore();
 //console.log(store.getState());
 
-
-store.dispatch(addExpense({description: 'Water bill', amount: 4500}))
-store.dispatch(addExpense({ description: 'Gas bill', amount: 400, createdAt: 1000 }))
-store.dispatch(addExpense({ description: 'Rent', amount: 55500, createdAt: -1000 }))
-
-store.subscribe(() => {
-    const state = store.getState()
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-    console.log(visibleExpenses)
-})
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
