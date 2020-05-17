4948ffbe35f83c198ab34c97c52785fa5e7b52ba
sungnga <nga422@gmail.com>
2020-05-16 23:27:39 -0700

    Mocking library with jest: mocking moment()

--- reactjs/expensify-app/src/app.js
+++ reactjs/expensify-app/src/app.js
@@ -5,6 +5,7 @@ import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 //console.log(store.getState());
