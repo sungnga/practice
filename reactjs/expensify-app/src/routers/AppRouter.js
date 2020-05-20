436117311508f8202affcbbffe36f9bfe86bd55a
sungnga <nga422@gmail.com>
2020-05-20 15:56:10 -0700

    Add private route

--- reactjs/expensify-app/src/routers/AppRouter.js
+++ reactjs/expensify-app/src/routers/AppRouter.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import {createBrowserHistory} from 'history';
-import Header from '../components/Header';
 import NotFoundPage from '../components/NotFoundPage';
 import HelpPage from '../components/HelpPage';
 import EditExpensePage from '../components/EditExpensePage';
 import AddExpensePage from '../components/AddExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createBrowserHistory();
 
 const AppRouter = () => (
     <Router history={history}>
         <div>
-            <Header />
             <Switch>
                 <Route path="/" component={LoginPage} exact={true} />
-                <Route path="/dashboard" component={ExpenseDashboardPage} />
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit/:id" component={EditExpensePage} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+                <PrivateRoute path="/create" component={AddExpensePage} />
+                <PrivateRoute path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
