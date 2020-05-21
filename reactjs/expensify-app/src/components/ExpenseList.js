07dee30858d0e7f1732cdaa2a6d7593c3c804f4b
sungnga <nga422@gmail.com>
2020-05-21 11:53:16 -0700

    Styling expenses list

--- reactjs/expensify-app/src/components/ExpenseList.js
+++ reactjs/expensify-app/src/components/ExpenseList.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import ExpenseListItem from './ExpenseListItem'
-import selectExpenses from '../selectors/expenses'
+import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../selectors/expenses';
 
 // A stateless functional component
 export const ExpenseList = (props) => {
     //console.log(props)
     return (
-        <div>
-            {
-                props.expenses.length === 0 ? (
-                    <p>No expenses</p>
+        <div className='content-container'>
+            <div className='list-header'>
+                <div className='show-for-mobile'>Expenses</div>
+                <div className='show-for-desktop'>Expense</div>
+                <div className='show-for-desktop'>Amount</div>
+            </div>
+            <div className="list-body">
+                {props.expenses.length === 0 ? (
+                    <div className='list-item list-item--message'>
+                        <span>No expenses</span>
+                    </div>
                 ) : (
                     props.expenses.map((expense) => {
-                        return <ExpenseListItem key={expense.id} {...expense} />  
+                        return (
+                            <ExpenseListItem key={expense.id} {...expense} />
+                        );
                     })
-                )
-            }
+                )}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 // Create a HOC
 // We need to pass in a regular component to the connect function
@@ -29,15 +38,15 @@ export const ExpenseList = (props) => {
 // The store state actually gets passed in as 1st arg
 // From this function, we return an object with key/value pair as props
 // ConnectedExpenseList is the HOC of the ExpenseList component
-// We export the HOC 
+// We export the HOC
 
 // STANDARD VERSION:
 // A function that maps state to props
 const mapStateToProps = (state) => {
     return {
-        expenses: selectExpenses(state.expenses, state.filters)
+        expenses: selectExpenses(state.expenses, state.filters),
     };
-}
+};
 
 export default connect(mapStateToProps)(ExpenseList);
 
