6d48b892477959fa42395097ea2997e58146234f
sungnga <nga422@gmail.com>
2020-06-09 14:40:47 -0700

    React state: dice and lottery exercises

--- reactjs/exercises/react-state-dice/src/App.js
+++ reactjs/exercises/react-state-dice/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Die from './Die'
-import RollDice from './RollDice';
+import './App.css'
+import RollDice from './dice/RollDice';
+import Lottery from './lotto/Lottery'
 
 function App() {
   return (
     <div className="App">
-      <RollDice />
+    <RollDice />
+      <Lottery />
+      <Lottery title="Mini Daily" maxNum={10} numBalls={4} /> 
     </div>
   );
 }
 
 export default App;
+
+
