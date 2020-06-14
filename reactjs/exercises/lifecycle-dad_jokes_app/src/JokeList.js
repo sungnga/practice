75cbf26b29de10210ba4ddf1b6f531e58d4b37fc
sungnga <nga422@gmail.com>
2020-06-14 09:26:47 -0700

    Style JokeList, create Joke comp, upvote/downvote

--- reactjs/exercises/lifecycle-dad_jokes_app/src/JokeList.js
+++ reactjs/exercises/lifecycle-dad_jokes_app/src/JokeList.js
@@ -1,35 +1,59 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+import './JokeList.css';
+import Joke from './Joke';
 
 class JokeList extends Component {
-  static defaultProps = {
-    numJokesToGet: 10
-  }
-  constructor(props) {
-    super(props)
-    this.state = {jokes: []}
-  }
-  async componentDidMount() {
-    // Load jokes
-    let jokes = [];
-    while (jokes.length < this.props.numJokesToGet) {
-      let res = await axios.get('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } })
-      jokes.push(res.data.joke)
-    }
-    this.setState({jokes: jokes})
-  }
-  render() {
-    return (
-      <div className="JokeList">
-        <h1>Joke List</h1>
-        <div className="JokeList-jokes">
-          {this.state.jokes.map(j => (
-            <div>{j}</div>
-          ))}
-        </div>
-      </div>
-    );
-  }
+	static defaultProps = {
+		numJokesToGet: 10
+	};
+	constructor(props) {
+		super(props);
+		this.state = { jokes: [] };
+	}
+	async componentDidMount() {
+		// Load jokes
+		let jokes = [];
+		while (jokes.length < this.props.numJokesToGet) {
+			let res = await axios.get('https://icanhazdadjoke.com/', {
+				headers: { Accept: 'application/json' }
+			});
+			jokes.push({ id: uuidv4(), text: res.data.joke, votes: 0 });
+		}
+		this.setState({ jokes: jokes });
+	}
+	handleVote(id, delta) {
+		this.setState((st) => ({
+			jokes: st.jokes.map((j) =>
+				j.id === id ? { ...j, votes: j.votes + delta } : j
+			)
+		}));
+	}
+	render() {
+		return (
+			<div className='JokeList'>
+				<div className='JokeList-sidebar'>
+					<h1 className='JokeList-title'>
+						<span>Dad</span> Jokes
+					</h1>
+					<img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+					<button className='JokeList-getmore'>New Jokes</button>
+				</div>
+				<div className='JokeList-jokes'>
+					{this.state.jokes.map((j) => (
+						<Joke
+							key={j.id}
+							votes={j.votes}
+							text={j.text}
+							upvote={() => this.handleVote(j.id, 1)}
+							downvote={() => this.handleVote(j.id, -1)}
+						/>
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default JokeList;
