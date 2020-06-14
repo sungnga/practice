238a8a567d415be4a2e4e5df377b4ecc29d13b0b
sungnga <nga422@gmail.com>
2020-06-14 13:10:27 -0700

    Add loading spinner, preventing duplicate jokes

--- reactjs/exercises/lifecycle-dad_jokes_app/src/JokeList.js
+++ reactjs/exercises/lifecycle-dad_jokes_app/src/JokeList.js
@@ -14,8 +14,11 @@ class JokeList extends Component {
     // Use JSON.parse() to convert string objects to JS objects
     // NOTE: 'jokes' inside .getItem('jokes') is the key
 		this.state = {
-			jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]')
-		};
+      jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'),
+      loading: false
+    };
+    this.seenJokes = new Set(this.state.jokes.map(j => j.text))
+    // console.log(this.seenJokes)
 		this.handleClick = this.handleClick.bind(this);
   }
   // In the beginning, if there's no jokes, call .getJokes()
@@ -24,26 +27,39 @@ class JokeList extends Component {
 			this.getJokes();
 		}
 	}
-	async getJokes() {
-		// Load jokes
-		let jokes = [];
-		while (jokes.length < this.props.numJokesToGet) {
-			let res = await axios.get('https://icanhazdadjoke.com/', {
-				headers: { Accept: 'application/json' }
-			});
-			jokes.push({ id: uuidv4(), text: res.data.joke, votes: 0 });
-		}
-		// setState to contain existing jokes and new jokes
-    // Once the state is set, it will save to local storage
-    // Local storage can only store objects but in string form
-    // Use JSON.stringify() to convert JS objects to string version
-		this.setState(
-			(st) => ({
-				jokes: [...st.jokes, ...jokes]
-			}),
-			() =>
-				window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
-		);
+  async getJokes() {
+    try {
+      // Load jokes
+      let jokes = [];
+      while (jokes.length < this.props.numJokesToGet) {
+        let res = await axios.get('https://icanhazdadjoke.com/', {
+          headers: { Accept: 'application/json' }
+        });
+        let newJoke = res.data.joke;
+        // If the new joke is not in the seenJokes set, push the new joke into the jokes array
+        if (!this.seenJokes.has(newJoke)) {
+          jokes.push({ id: uuidv4(), text: newJoke, votes: 0 });
+        } else {
+          console.log('FOUND A DUPLICATE')
+          console.log(newJoke)
+        }
+      }
+      // setState to contain existing jokes and new jokes
+      // Once the state is set, it will save to local storage
+      // Local storage can only store objects but in string form
+      // Use JSON.stringify() to convert JS objects to string version
+      this.setState(
+        (st) => ({
+          loading: false,
+          jokes: [...st.jokes, ...jokes]
+        }),
+        () =>
+          window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+      );
+    } catch (err) {
+      alert(err);
+      this.setState({loading: false})
+    }
   }
   // Once the updated vote is set in state, store the jokes list in local storage
 	handleVote(id, delta) {
@@ -56,11 +72,22 @@ class JokeList extends Component {
 			() =>
 				window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
 		);
-	}
+  }
+  // After loading is set to true, we want the callback function to run
+  // NOTE: we don't want to invoke the .getJokes() function inside the onClick event. We want to call it when the user clicks the button
 	handleClick() {
-		this.getJokes();
+		this.setState({loading: true}, this.getJokes)
 	}
-	render() {
+  render() {
+    // Return early if loading state is true
+    if (this.state.loading) {
+      return (
+        <div className="JokeList-spinner">
+          <i className="far fa-8x fa-laugh fa-spin" />
+          <h1 className="JokeList-title">Loading...</h1>
+        </div>
+      )
+    }
 		return (
 			<div className='JokeList'>
 				<div className='JokeList-sidebar'>
