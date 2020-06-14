5c5563b6d35745017693049f1b4cba4836ea2e49
sungnga <nga422@gmail.com>
2020-06-14 12:32:13 -0700

    Syncing with localStorage

--- reactjs/exercises/lifecycle-dad_jokes_app/src/JokeList.js
+++ reactjs/exercises/lifecycle-dad_jokes_app/src/JokeList.js
@@ -9,10 +9,22 @@ class JokeList extends Component {
 		numJokesToGet: 10
 	};
 	constructor(props) {
-		super(props);
-		this.state = { jokes: [] };
+    super(props);
+    // Retrieve jokes from local storage if there's any, else set to empty array
+    // Use JSON.parse() to convert string objects to JS objects
+    // NOTE: 'jokes' inside .getItem('jokes') is the key
+		this.state = {
+			jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]')
+		};
+		this.handleClick = this.handleClick.bind(this);
+  }
+  // In the beginning, if there's no jokes, call .getJokes()
+	componentDidMount() {
+		if (this.state.jokes.length === 0) {
+			this.getJokes();
+		}
 	}
-	async componentDidMount() {
+	async getJokes() {
 		// Load jokes
 		let jokes = [];
 		while (jokes.length < this.props.numJokesToGet) {
@@ -21,14 +33,32 @@ class JokeList extends Component {
 			});
 			jokes.push({ id: uuidv4(), text: res.data.joke, votes: 0 });
 		}
-		this.setState({ jokes: jokes });
-	}
+		// setState to contain existing jokes and new jokes
+    // Once the state is set, it will save to local storage
+    // Local storage can only store objects but in string form
+    // Use JSON.stringify() to convert JS objects to string version
+		this.setState(
+			(st) => ({
+				jokes: [...st.jokes, ...jokes]
+			}),
+			() =>
+				window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+		);
+  }
+  // Once the updated vote is set in state, store the jokes list in local storage
 	handleVote(id, delta) {
-		this.setState((st) => ({
-			jokes: st.jokes.map((j) =>
-				j.id === id ? { ...j, votes: j.votes + delta } : j
-			)
-		}));
+		this.setState(
+			(st) => ({
+				jokes: st.jokes.map((j) =>
+					j.id === id ? { ...j, votes: j.votes + delta } : j
+				)
+			}),
+			() =>
+				window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+		);
+	}
+	handleClick() {
+		this.getJokes();
 	}
 	render() {
 		return (
@@ -38,7 +68,9 @@ class JokeList extends Component {
 						<span>Dad</span> Jokes
 					</h1>
 					<img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
-					<button className='JokeList-getmore'>New Jokes</button>
+					<button onClick={this.handleClick} className='JokeList-getmore'>
+						New Jokes
+					</button>
 				</div>
 				<div className='JokeList-jokes'>
 					{this.state.jokes.map((j) => (
