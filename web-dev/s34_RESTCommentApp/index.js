04a88dcbe42633a8534dad85738003c8dc6ad6ba
sungnga <nga422@gmail.com>
2020-11-14 23:58:50 -0800

    Add RESTful GET route for comments index

--- web-dev/s34_RESTCommentApp/index.js
+++ web-dev/s34_RESTCommentApp/index.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+
+const comments = [
+	{
+		username: 'Todd',
+		comment: 'lol that is so funny!'
+	},
+	{
+		username: 'Skylar',
+		comment: 'I like to go birdwatching'
+	},
+	{
+		username: 'Skateboy',
+		comment: 'Plz delete your account, Todd'
+	},
+	{
+		username: 'onlysayswoof',
+		comment: 'woof woof'
+	},
+]
+
+app.get('/comments', (req, res) => {
+	res.render('comments/index', {comments})
+})
 
 app.get('/tacos', (req, res) => {
 	res.send('GET /tacos response');
