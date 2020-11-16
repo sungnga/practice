d3b31418219b2573b58055e283ee17a70c63f155
sungnga <nga422@gmail.com>
2020-11-15 21:25:04 -0800

    Create route for RESTful comments new

--- web-dev/s34_RESTCommentApp/index.js
+++ web-dev/s34_RESTCommentApp/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set('views', path.join(__dirname, '/views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 
 const comments = [
 	{
@@ -23,11 +23,23 @@ const comments = [
 	{
 		username: 'onlysayswoof',
 		comment: 'woof woof'
-	},
-]
+	}
+];
 
 app.get('/comments', (req, res) => {
-	res.render('comments/index', {comments})
+	res.render('comments/index', { comments });
+});
+
+// This route serves up the form
+app.get('/comments/new', (req, res) => {
+	res.render('comments/new')
+})
+
+// This route takes the data from the form and sends it to the server
+app.post('/comments', (req, res) => {
+	const { username, comment } = req.body
+	comments.push({username, comment})
+	res.send('it worked!')
 })
 
 app.get('/tacos', (req, res) => {
