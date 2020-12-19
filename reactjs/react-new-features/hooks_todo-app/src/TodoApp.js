17eda42b24da133286931130818d0dcd41b6fb9e
sungnga <nga422@gmail.com>
2020-12-19 09:29:17 -0800

    Adding Todo item component, add styling  to form

--- reactjs/react-new-features/hooks_todo-app/src/TodoApp.js
+++ reactjs/react-new-features/hooks_todo-app/src/TodoApp.js
@@ -6,7 +6,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 
 import TodoList from './TodoList';
-import TodoForm from './TodoForm'
+import TodoForm from './TodoForm';
 
 function TodoApp() {
 	const initialTodos = [
@@ -14,11 +14,11 @@ function TodoApp() {
 		{ id: 2, task: 'Wash dishes', completed: true },
 		{ id: 3, task: 'Cook dinner', completed: false }
 	];
-  const [todos, setTodos] = useState(initialTodos);
-  const addTodo = newTodoText => {
-    setTodos([...todos, {id: 4, task: newTodoText, completed: false}])
-  }
-  console.log(todos)
+	const [todos, setTodos] = useState(initialTodos);
+	const addTodo = (newTodoText) => {
+		setTodos([...todos, { id: 4, task: newTodoText, completed: false }]);
+	};
+	console.log(todos);
 	return (
 		<Paper
 			style={{
@@ -33,9 +33,13 @@ function TodoApp() {
 				<Toolbar>
 					<Typography color='inherit'>TODOS WITH HOOKS</Typography>
 				</Toolbar>
-      </AppBar>
-      <TodoForm addTodo={addTodo} />
-			<TodoList todos={todos} />
+			</AppBar>
+			<Grid container justify='center' style={{marginTop: '1rem'}}>
+				<Grid item xs={11} md={8} lg={4}>
+					<TodoForm addTodo={addTodo} />
+					<TodoList todos={todos} />
+				</Grid>
+			</Grid>
 		</Paper>
 	);
 }
