8eed5e3b8db0fedf81bb559fa9ca32fa810940c1
sungnga <nga422@gmail.com>
2020-12-19 21:26:23 -0800

    Adding localStorage with useEffect hook

--- reactjs/react-new-features/hooks_todo-app/src/TodoApp.js
+++ reactjs/react-new-features/hooks_todo-app/src/TodoApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
@@ -10,12 +10,18 @@ import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 
 function TodoApp() {
-	const initialTodos = [
-		{ id: 1, task: 'Clean room', completed: false },
-		{ id: 2, task: 'Wash dishes', completed: true },
-		{ id: 3, task: 'Cook dinner', completed: false }
-	];
+	const initialTodos = JSON.parse(window.localStorage.getItem('todos') || '[]');
+	// const initialTodos = [
+	// 	{ id: 1, task: 'Clean room', completed: false },
+	// 	{ id: 2, task: 'Wash dishes', completed: true },
+	// 	{ id: 3, task: 'Cook dinner', completed: false }
+	// ];
 	const [todos, setTodos] = useState(initialTodos);
+
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
+
 	const addTodo = (newTodoText) => {
 		setTodos([...todos, { id: uuidv4(), task: newTodoText, completed: false }]);
 	};
