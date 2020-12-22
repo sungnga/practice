8bdba9fa8889650584cbeebb8cc4dc4b19733407
sungnga <nga422@gmail.com>
2020-12-22 07:28:54 -0800

    Rewriting ThemeContext Provider with custom hook

--- reactjs/react-new-features/context-hooks_language-app-v2/src/contexts/ThemeContext.js
+++ reactjs/react-new-features/context-hooks_language-app-v2/src/contexts/ThemeContext.js
@@ -1,4 +1,5 @@
-import React, { Component, createContext } from 'react';
+import React, { createContext } from 'react';
+import useToggleState from '../hooks/useToggleState';
 
 // Creating a context object. Save it to a variable
 // Every context object comes with a Provider and a Consumer
@@ -6,27 +7,16 @@ export const ThemeContext = createContext();
 
 // Define a Provider component
 // The Provider contains the state that other components can subscribe to have access to this state and any context changes
-export class ThemeProvider extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { isDarkMode: true };
-		this.toggleTheme = this.toggleTheme.bind(this);
-	}
+export function ThemeProvider(props) {
+	// Using custom hook to toggle state
+	const [isDarkMode, toggleTheme] = useToggleState(false);
 
-	toggleTheme() {
-		this.setState({ isDarkMode: !this.state.isDarkMode });
-	}
-
-	render() {
-		// Any components inside the Provider wrapper will have access to the value prop and subscribe to context changes
-		return (
-			<ThemeContext.Provider
-				value={{ ...this.state, toggleTheme: this.toggleTheme }}
-			>
-				{this.props.children}
-			</ThemeContext.Provider>
-		);
-	}
+	// Any components inside the Provider wrapper will have access to the value prop and subscribe to context changes
+	return (
+		<ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+			{props.children}
+		</ThemeContext.Provider>
+	);
 }
 
 // NOTES
@@ -37,13 +27,3 @@ export class ThemeProvider extends Component {
 // One provider can be connected to many consumers
 // Providers can be nested to override values deeper within the tree
 // All consumers that are descendants of a Provider will re-render whenever the Provider's `value` prop changes
-
-// TO CONSUME THE CONTEXT:
-// Go to any of the consuming component, set the contextType to the name of the context object
-//   import ThemeContext context object. Then set the contextType
-//   static contextType = ThemeContext;
-// The `value` prop can be accessed using this.context
-//   const { isDarkMode } = this.context
-// Now the state/dataset stored in the context Provider can be used anywhere in the component that subscribes to it
-// Another way to consume a context is by using MyContext.Consumer
-// Look in Navbar.js and LanguageContext.js files to see how this used
