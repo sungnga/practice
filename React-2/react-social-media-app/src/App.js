925793230701ababa6f7da1011724ab963197029
sungnga <nga422@gmail.com>
2021-01-07 12:24:15 -0800

    Creating a simple user login/logout functionality

--- React-2/react-social-media-app/src/App.js
+++ React-2/react-social-media-app/src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import Login from './components/Login';
+import Header from './components/Header';
+import CreatePost from './components/CreatePost';
 
 function App() {
-	return <div>App</div>;
+	const [user, setUser] = useState('Nga');
+
+	useEffect(() => {
+		document.title = user ? `${user}'s Feed` : 'Please login';
+	}, [user]);
+
+	if (!user) {
+		return <Login setUser={setUser} />;
+	}
+	return (
+		<Fragment>
+			<Header user={user} setUser={setUser} />
+		</Fragment>
+	);
 }
 
 export default App;
--- React-2/react-social-media-app/src/components/Login.js
+++ React-2/react-social-media-app/src/components/Login.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Login() {
-	return <div>Login</div>;
+function Login({ setUser }) {
+	const [username, setUsername] = useState('');
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		setUser(username);
+	}
+	return (
+		<div>
+			<h2>Login</h2>
+			<form onSubmit={handleSubmit}>
+				<input
+					onChange={(event) => setUsername(event.target.value)}
+					placeholder='Input username'
+				/>
+				<button type='submit'>Submit</button>
+			</form>
+		</div>
+	);
 }
 
 export default Login;
