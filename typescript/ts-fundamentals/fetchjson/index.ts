f18d9f06861e909d0d0a0918ca8dc39f695a009e
sungnga <nga422@gmail.com>
2021-03-08 10:15:39 -0800

    Fixing errors with typescript, interface

--- typescript/ts-fundamentals/fetchjson/index.ts
+++ typescript/ts-fundamentals/fetchjson/index.ts
@@ -2,17 +2,25 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+// Interfaces in ts are used to define the structure of an object
+// Define the type for each property of an object
+interface Todo {
+	id: number;
+	title: string;
+	completed: boolean;
+}
+
 axios.get(url).then((response) => {
 	// console.log(response.data)
-  const todo = response.data;
-  
-  const ID = todo.ID
-  const title = todo.Title
-  const finished = todo.finished
+	const todo = response.data as Todo;
 
-  console.log(`
-    The Todo with ID: ${ID}
+	const id = todo.id;
+	const title = todo.title;
+	const completed = todo.completed;
+
+	console.log(`
+    The Todo with ID: ${id}
     Has a title of: ${title}
-    Is it finished? ${finished}
-  `)
+    Is it finished? ${completed}
+  `);
 });
