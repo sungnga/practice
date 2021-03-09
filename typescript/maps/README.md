13439a833c2d2c627ccda23b5f20407f0baa4ad0
sungnga <nga422@gmail.com>
2021-03-09 13:25:08 -0800

    Integrating Google maps with typescript

--- typescript/maps/README.md
+++ typescript/maps/README.md
@@ -1,5 +1,7 @@
 ### Installing parcel-bundler parcelV2
-- Install: `npm install -D parcel@next`
+- Install: `npm install -D parcel@next` parcelV2 is in beta
+- Or install: `npm i -D parcel-bundler@1.12.3`
+- Install globally: `sudo npm i -g parcel-bundler@1.12.3`
 - Tool to help us run Typescript in the browser
 
 ### Setup Parcel
@@ -37,3 +39,15 @@
 
 #### 2. Defining a Company class with faker library
 - Import the Company.ts class in index.ts file
+
+#### 3. Integrating Google Maps with TypeScript
+- Google maps with typescript examples: https://developers.google.com/maps/documentation/javascript/examples
+- Generate a Google Dev Project in Google Developer console
+  - Select Maps Javascript API service
+  - Enable Google Maps support inside the project
+  - Generate an API key
+- Add the google maps script tag to index.html file
+- Install @types/googlemaps type definition file: `npm i -D @types/googlemaps`
+- Using the google.maps type definition in index.ts
+- In index.html file, add a div tag with an id of 'map'
+- Create an index.css file and insert it into index.html file
--- typescript/maps/src/index.ts
+++ typescript/maps/src/index.ts
@@ -1,8 +1,58 @@
-import { User } from './User';
-import { Company } from './Company';
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user = new User();
-console.log(user);
+// const user = new User();
+// console.log(user);
 
-const company = new Company();
-console.log(company);
+// const company = new Company();
+// console.log(company);
+
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+})
+
+
+
+
+
+
+
+// let map: google.maps.Map;
+
+// function initMap(): void {
+//   const mapOptions = {
+//     zoom: 8,
+//     center: { lat: -34.397, lng: 150.644 },
+//   };
+//   map = new google.maps.Map(
+//     document.getElementById("map") as HTMLElement,
+//     mapOptions
+//   );
+
+//   const marker = new google.maps.Marker({
+//     // The below line is equivalent to writing:
+//     // position: new google.maps.LatLng(-34.397, 150.644)
+//     position: { lat: -34.397, lng: 150.644 },
+//     map: map,
+//   });
+
+//   // You can use a LatLng literal in place of a google.maps.LatLng object when
+//   // creating the Marker object. Once the Marker object is instantiated, its
+//   // position will be available as a google.maps.LatLng object. In this case,
+//   // we retrieve the marker's position using the
+//   // google.maps.LatLng.getPosition() method.
+//   const infowindow = new google.maps.InfoWindow({
+//     content: "<p>Marker Location:" + marker.getPosition() + "</p>",
+//   });
+
+//   google.maps.event.addListener(marker, "click", () => {
+//     infowindow.open(map, marker);
+//   });
+// }
+
+// initMap()
