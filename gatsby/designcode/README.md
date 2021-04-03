e028a912e8aaf0b38c3715984238f2efdb437ee8
sungnga <nga422@gmail.com>
2021-04-03 12:31:34 -0700

    Initialize project with starter files

---
+++ gatsby/designcode/README.md
@@ -0,0 +1,21 @@
+### Project starter file structure:
+  .
+  ├── src
+  ├──── components
+  ├────── layout
+  ├──────── layout.css
+  ├──────── layout.js
+  ├──────── seo.js
+  ├──── pages
+  ├────── 404.js
+  ├────── index.js
+  ├── static
+  ├──── images
+  ├── gatsby-browser.js
+  ├── gatsby-config.js
+  ├── gatsby-node.js
+  ├── gatsby-ssr.js
+  ├── package-lock.json
+  ├── package.json
+  └── README.md
+
---
+++ gatsby/designcode/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteMetadata: {
+    title: `Design+Code`,
+    description: `Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete courses about the best tools.`,
+    author: `@_ngala`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/images`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
