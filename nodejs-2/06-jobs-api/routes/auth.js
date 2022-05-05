e5b2ce9648828b3b10cad665849003f3f9519cba
sungnga <nga422@gmail.com>
2022-05-05 16:36:31 -0700

    06-jobs-api: Create user - basic setup

--- nodejs-2/06-jobs-api/routes/auth.js
+++ nodejs-2/06-jobs-api/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { login, register } = require('../controllers/auth');
 
+// the base route is '/api/v1/auth' and was setup in app.js file
 router.post('/register', register);
 router.post('/login', login);
 
