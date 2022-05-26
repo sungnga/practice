b029b6996db8437a6b77aad8a0f7fa38dfa05fd4
sungnga <nga422@gmail.com>
2022-05-25 23:58:36 -0700

    06-jobs-api: Implement updateJob controller

--- nodejs-2/06-jobs-api/controllers/jobs.js
+++ nodejs-2/06-jobs-api/controllers/jobs.js
@@ -38,7 +38,28 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-	res.send('update job');
+	// destructure from req object
+	const {
+		body: { company, position },
+		user: { userId },
+		params: { id: jobId }
+	} = req;
+
+	if (company === '' || position === '') {
+		throw new BadRequestError('Company or Position fields cannot be empty');
+  }
+  
+	// set new to true means it will return the updated job
+	const job = await Job.findByIdAndUpdate(
+		{ _id: jobId, createdBy: userId },
+		req.body,
+		{ new: true, runValidators: true }
+	);
+	if (!job) {
+		throw new NotFoundError(`No job with id ${jobId}`);
+	}
+
+	res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
