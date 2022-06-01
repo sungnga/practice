d4b8dabf76c47ef05751c8441657d6ef85a4e936
sungnga <nga422@gmail.com>
2022-05-31 19:14:26 -0700

    06-jobs-api: Setup security, install packages

--- nodejs-2/06-jobs-api/app.js
+++ nodejs-2/06-jobs-api/app.js
@@ -13,12 +13,28 @@ const authenticateUser = require('./middleware/authentication');
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
 
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('rate-limiter');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx)
+app.set('trust proxy', 1);
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000, //15 minutes
+		max: 100 //limit each IP to 100 requests per windowMs
+	})
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 // base route for auth
