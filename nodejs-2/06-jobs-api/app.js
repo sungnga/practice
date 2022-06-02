92f70f1ff7c3e10b3f1fda738ad757b3c158ac62
sungnga <nga422@gmail.com>
2022-06-01 20:37:12 -0700

    06-jobs-api: Deploy app to Heroku

--- nodejs-2/06-jobs-api/app.js
+++ nodejs-2/06-jobs-api/app.js
@@ -36,6 +36,11 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+// testing the root route
+app.get('/', (req, res) => {
+	res.send('jobs api');
+});
+
 // routes
 // base route for auth
 app.use('/api/v1/auth', authRouter);
