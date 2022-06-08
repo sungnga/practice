d8e06c87665673601b685b8e12cbd29eada1a816
sungnga <nga422@gmail.com>
2022-06-07 23:59:44 -0700

    07-file-upload: Upload image file using Postman

--- nodejs-2/07-file-upload/controllers/uploadsController.js
+++ nodejs-2/07-file-upload/controllers/uploadsController.js
@@ -1,7 +1,21 @@
+const path = require('path');
 const { StatusCodes } = require('http-status-codes');
 
 const uploadProductImage = async (req, res) => {
-	res.send('upload product image');
+	console.log(req.files);
+	let productImage = req.files.image;
+
+	// create a path in uploads folder
+	const imagePath = path.join(
+		__dirname,
+		'../public/uploads/' + `${productImage.name}`
+	);
+	// moving image data to the imagePath in uploads folder
+	// the .mv() method is provided by express-fileupload library
+	await productImage.mv(imagePath);
+	return res
+		.status(StatusCodes.OK)
+		.json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 module.exports = { uploadProductImage };
