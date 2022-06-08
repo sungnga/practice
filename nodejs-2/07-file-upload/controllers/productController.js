4519fc8171c5b3069d10213dcc3011f6bbb457f5
sungnga <nga422@gmail.com>
2022-06-08 00:50:20 -0700

    07-file: Implement createProduct, getAllProducts

--- nodejs-2/07-file-upload/controllers/productController.js
+++ nodejs-2/07-file-upload/controllers/productController.js
@@ -2,11 +2,15 @@ const Product = require('../models/Product');
 const { StatusCodes } = require('http-status-codes');
 
 const createProduct = async (req, res) => {
-	res.send('create product');
+	console.log(req.body);
+	const product = await Product.create(req.body);
+	res.status(StatusCodes.CREATED).json({ product });
 };
 
 const getAllProducts = async (req, res) => {
-	res.send('list of products');
+	// passing in empty object {} will find all the products
+	const products = await Product.find({});
+	res.status(StatusCodes.OK).json({ products });
 };
 
 module.exports = { createProduct, getAllProducts };
