80a9e2eee6050af8a82375260519e366a69d5c40
sungnga <nga422@gmail.com>
2022-06-09 19:02:48 -0700

    07-file: Additional validation for image upload

--- nodejs-2/07-file-upload/controllers/uploadsController.js
+++ nodejs-2/07-file-upload/controllers/uploadsController.js
@@ -1,9 +1,31 @@
 const path = require('path');
 const { StatusCodes } = require('http-status-codes');
+// we're getting the entire error object from errors/index.js file
+const CustomError = require('../errors');
 
 const uploadProductImage = async (req, res) => {
-	console.log(req.files);
-	let productImage = req.files.image;
+	// console.log(req.files);
+
+	// check if file exists
+	if (!req.files) {
+		throw new CustomError.BadRequestError('No file uploaded');
+	}
+
+	const productImage = req.files.image;
+
+	// check format
+	if (!productImage.mimetype.startsWith('image')) {
+		throw new CustomError.BadRequestError('Please upload image');
+	}
+
+	// maxSize is 1MB
+	const maxSize = 1024 * 1024;
+	// check size
+	if (productImage.size > maxSize) {
+		throw new CustomError.BadRequestError(
+			'Please upload image smaller than 1KB'
+		);
+	}
 
 	// create a path in uploads folder
 	const imagePath = path.join(
