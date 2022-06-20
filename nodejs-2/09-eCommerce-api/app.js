0807ab160cee65ebe46647f735db1cd833b706de
sungnga <nga422@gmail.com>
2022-06-19 23:27:53 -0700

    09-eCom-api: Connect to database

--- nodejs-2/09-eCommerce-api/app.js
+++ nodejs-2/09-eCommerce-api/app.js
@@ -1,15 +1,20 @@
+require('dotenv').config();
+
 // express
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 5000;
+// database
+const connectDB = require('./db/connect');
 
+const port = process.env.PORT || 5000;
 const start = async () => {
 	try {
+		// connect to MongoDB
+		await connectDB(process.env.MONGO_URL);
 		app.listen(port, console.log(`Server is listening on port ${port}...`));
 	} catch (error) {
 		console.log(error);
 	}
 };
-
 start();
