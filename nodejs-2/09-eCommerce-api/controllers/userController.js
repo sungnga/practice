00f46b09151eb9083f34748d16735a857d1b2c49
sungnga <nga422@gmail.com>
2022-07-07 23:22:32 -0700

    09-eCom-api: getAllUsers getSingleUser controllers

--- nodejs-2/09-eCommerce-api/controllers/userController.js
+++ nodejs-2/09-eCommerce-api/controllers/userController.js
@@ -1,10 +1,25 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
 // only admin users have access to this route
 const getAllUsers = async (req, res) => {
-	res.send('get all users route');
+	// get all users with role of 'user'
+	// then remove the password property from the return array of users
+	const users = await User.find({ role: 'user' }).select('-password');
+
+	res.status(StatusCodes.OK).json({ users });
 };
 
 const getSingleUser = async (req, res) => {
-	res.send(req.params);
+	// find one user that has the match id from req.params.id
+	// then remove the password property from the found user object
+	const user = await User.findOne({ _id: req.params.id }).select('-password');
+	if (!user) {
+		throw new CustomError.NotFoundError(`No user with id: ${req.params.id}`);
+	}
+
+	res.status(StatusCodes.OK).json({ user });
 };
 
 const showCurrentUser = async (req, res) => {
