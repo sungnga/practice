d64784eab02f52b1368fe5f18d41195733c795b9
sungnga <nga422@gmail.com>
2022-07-26 23:10:30 -0700

    09-eCom-api: Implement createReview controller

--- nodejs-2/09-eCommerce-api/controllers/reviewController.js
+++ nodejs-2/09-eCommerce-api/controllers/reviewController.js
@@ -1,5 +1,37 @@
+const Review = require('../models/Review');
+const Product = require('../models/Product');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const { checkPermissions } = require('../utils');
+
 const createReview = async (req, res) => {
-	res.send('Create review');
+	// destructure product and assign an alias to productId
+	const { product: productId } = req.body;
+
+	// check if the submitted product exists
+	const isValidProduct = await Product.findOne({ _id: productId });
+	if (!isValidProduct) {
+		throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+	}
+
+	// check if the review has already been submitted by
+	// this user on this product
+	const alreadySubmitted = await Review.findOne({
+		product: productId,
+		user: req.user.userId
+	});
+	if (alreadySubmitted) {
+		throw new CustomError.BadRequestError(
+			'Already submitted review for this product'
+		);
+	}
+
+	// add user property to req.body
+	req.body.user = req.user.userId;
+	// create review
+	const review = await Review.create(req.body);
+
+	res.status(StatusCodes.CREATED).json({ review });
 };
 
 const getAllReviews = async (req, res) => {
