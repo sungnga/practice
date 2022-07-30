5b463b9225e8255b26c4dc9a0be7231d8c83c29a
sungnga <nga422@gmail.com>
2022-07-29 22:57:51 -0700

    09-eCom-api: Implement updateReview controller

--- nodejs-2/09-eCommerce-api/controllers/reviewController.js
+++ nodejs-2/09-eCommerce-api/controllers/reviewController.js
@@ -54,7 +54,23 @@ const getSingleReview = async (req, res) => {
 };
 
 const updateReview = async (req, res) => {
-	res.send('update review');
+	const { id: reviewId } = req.params;
+	const { rating, title, comment } = req.body;
+
+	const review = await Review.findOne({ _id: reviewId });
+	if (!review) {
+		throw new CustomError.NotFoundError(`No review with id: ${reviewId}`);
+	}
+
+	checkPermissions(req.user, review.user);
+
+	review.rating = rating;
+	review.title = title;
+	review.comment = comment;
+
+	await review.save();
+
+	res.status(StatusCodes.OK).json({ review });
 };
 
 const deleteReview = async (req, res) => {
@@ -65,7 +81,7 @@ const deleteReview = async (req, res) => {
 		throw new CustomError.NotFoundError(`No review with id: ${reviewId}`);
 	}
 
-  checkPermissions(req.user, review.user);
+	checkPermissions(req.user, review.user);
 
 	await review.remove();
 
