b3e329bda3002111288d3e4ee2aeb65ec30045a7
sungnga <nga422@gmail.com>
2022-08-06 14:37:52 -0700

    09-eCom-api: Setup Order routes structure

--- nodejs-2/09-eCommerce-api/app.js
+++ nodejs-2/09-eCommerce-api/app.js
@@ -20,6 +20,7 @@ const authRouter = require('./routes/authRoutes.js');
 const userRouter = require('./routes/userRoutes');
 const productRouter = require('./routes/productRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const orderRouter = require('./routes/orderRoutes');
 
 // middleware
 const notFoundMiddleware = require('./middleware/not-found');
@@ -55,6 +56,7 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/vi/orders', orderRouter);
 
 // the 404 error handler is placed after all the routes and before other error handlers
 // because this middleware doesn't call next(). Everything ends after this
