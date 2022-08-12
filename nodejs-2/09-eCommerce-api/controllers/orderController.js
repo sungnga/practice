7db74a9d5e4a1a85ba7e91e22cdce5101409eabf
sungnga <nga422@gmail.com>
2022-08-11 23:58:48 -0700

    09-eCom: Implement the rest of order controllers

--- nodejs-2/09-eCommerce-api/controllers/orderController.js
+++ nodejs-2/09-eCommerce-api/controllers/orderController.js
@@ -2,13 +2,12 @@ const Order = require('../models/Order');
 const Product = require('../models/Product');
 
 const CustomError = require('../errors');
-const { checkPermissions } = require('../utils/checkPermissions');
+const { checkPermissions } = require('../utils');
 const { StatusCodes } = require('http-status-codes');
 
 // a fake Stripe function that returns these values
 const fakeStripeAPI = async ({ amount, currency }) => {
 	const client_secret = 'someRandomValue';
-
 	return { client_secret, amount };
 };
 
@@ -80,20 +79,53 @@ const createOrder = async (req, res) => {
 		.json({ order, clientSecret: order.clientSecret });
 };
 
+// this route is for admin user only
+// the authorizePermissions middleware is applied to this route
 const getAllOrders = async (req, res) => {
-	res.send('get all orders');
+	const orders = await Order.find({});
+
+	res.status(StatusCodes.OK).json({ orders, count: orders.length });
 };
 
 const getSingleOrder = async (req, res) => {
-	res.send('get single order');
+	const { id: orderId } = req.params;
+	const order = await Order.findOne({ _id: orderId });
+	if (!order) {
+		throw new CustomError.NotFoundError(`No order with id: ${orderId}`);
+	}
+
+	// check if current user is equal to the user of the order
+	// req.user is the current user
+	checkPermissions(req.user, order.user);
+
+	res.status(StatusCodes.OK).json({ order });
 };
 
 const getCurrentUserOrders = async (req, res) => {
-	res.send('get current user orders');
+	// find all orders based on the current userId
+	// Remember, we have access to the req.user object because
+	// we applied the authenticateUser middleware to this route
+	const orders = await Order.find({ user: req.user.userId });
+
+	res.status(StatusCodes.OK).json({ orders, count: orders.length });
 };
 
 const updateOrder = async (req, res) => {
-	res.send('update order');
+	const { id: orderId } = req.params;
+	const { paymentIntentId } = req.body;
+
+	const order = await Order.findOne({ _id: orderId });
+	if (!order) {
+		throw new CustomError.NotFoundError(`No order with id: ${orderId}`);
+	}
+
+	checkPermissions(req.user, order.order);
+
+	order.paymentIntentId = paymentIntentId;
+	order.status = 'paid';
+	await order.save();
+
+	res.status(StatusCodes.OK).json({ order });
 };
 
 module.exports = {
