1c745d4001493477f7516e634a32946c1ad699fd
sungnga <nga422@gmail.com>
2022-08-20 23:16:03 -0700

    10-auth-workflow: verifyEmail controller & route

--- nodejs-2/10-auth-workflow/server/controllers/authController.js
+++ nodejs-2/10-auth-workflow/server/controllers/authController.js
@@ -33,6 +33,28 @@ const register = async (req, res) => {
 	});
 };
 
+const verifyEmail = async (req, res) => {
+	const { verificationToken, email } = req.body;
+	const user = await User.findOne({ email });
+
+	if (!user) {
+		throw new CustomError.UnauthenticatedError('Verification failed');
+	}
+
+	if (user.verificationToken !== verificationToken) {
+		throw new CustomError.UnauthenticatedError('Verification failed');
+	}
+
+	// if verify email successful, set the user instance
+	user.isVerified = true;
+	user.verified = Date.now();
+	user.verificationToken = verificationToken;
+
+	await user.save();
+
+	res.status(StatusCodes.OK).json({ msg: 'Email verified' });
+};
+
 const login = async (req, res) => {
 	const { email, password } = req.body;
 	if (!email || !password) {
@@ -58,6 +80,7 @@ const login = async (req, res) => {
 
 	res.status(StatusCodes.OK).json({ user: tokenUser });
 };
+
 const logout = async (req, res) => {
 	res.cookie('token', 'logout', {
 		httpOnly: true,
@@ -69,5 +92,6 @@ const logout = async (req, res) => {
 module.exports = {
 	register,
 	login,
-	logout
+	logout,
+	verifyEmail
 };
